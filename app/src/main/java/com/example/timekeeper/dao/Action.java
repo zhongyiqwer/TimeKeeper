98c7d8f67bb308a5dedbc48d39ea678e7679c17d
zjx <781433428@qq.com>
2018-05-22 17:16:35 +0800

    dao

--- app/src/main/java/com/example/timekeeper/dao/Action.java
+++ app/src/main/java/com/example/timekeeper/dao/Action.java
@@ -5,4 +5,79 @@ package com.example.timekeeper.dao;
  */
 
 public class Action {
+    private int id;
+    private String name;
+    private int timeLen;
+    private String des;
+    private int cretor;
+    private String [] times;
+    private int suit;
+    private int [] partPerson;
+
+    public Action() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTimeLen() {
+        return timeLen;
+    }
+
+    public void setTimeLen(int timeLen) {
+        this.timeLen = timeLen;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public int getCretor() {
+        return cretor;
+    }
+
+    public void setCretor(int cretor) {
+        this.cretor = cretor;
+    }
+
+    public String[] getTimes() {
+        return times;
+    }
+
+    public void setTimes(String[] times) {
+        this.times = times;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public void setSuit(int suit) {
+        this.suit = suit;
+    }
+
+    public int[] getPartPerson() {
+        return partPerson;
+    }
+
+    public void setPartPerson(int[] partPerson) {
+        this.partPerson = partPerson;
+    }
 }
